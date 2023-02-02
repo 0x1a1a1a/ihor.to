9a20f55c3cf10dd15021d6813d276f00da90dc08
inununit <59264318+inununit@users.noreply.github.com>
2023-02-02 15:49:40 +0200

    Restructured folders

---
+++ phrases.js
@@ -0,0 +1,13 @@
+const myList = [
+    "I am a designer who thinks",
+    "I am a software product designer",
+    "I am a digital product designer",
+    "I focused on problem investigation, user experience improvement, user flows and documentation",
+    "I help digital products to meet business goals and user needs simultaneously",
+    "I make everything handy",
+    "I am a 🇺🇦 proud Ukrainian",
+    "I am about consistency, iterations and love",
+    "🕵️‍♂️+👨‍🔬+👨‍💻"];
+function randomizeFunction() {
+    document.getElementById("randomPhrase").innerHTML = myList[Math.floor(Math.random() * myList.length)]
+}
