b5b271dd5b5554ef279c5ef995e04aa21a505e89
inununit <59264318+inununit@users.noreply.github.com>
2023-02-14 00:38:30 +0200

    Added CV section

--- js/phrases.js
+++ js/phrases.js
@@ -3,11 +3,13 @@ const myList = [
     "I am a software product designer",
     "I am a digital product designer",
     "I focused on problem investigation, user experience improvement, user flows and documentation",
+    "I make valuable digital tools for people more usable, safe, accessible and pleasant to use",
     "I help digital products to meet business goals and user needs simultaneously",
     "I make everything handy",
     "I am a proud Ukrainian 🇺🇦",
     "I am about consistency, iterations and love to details",
     "A designer who has all layers named",
+    "Problem-first and evidence-based approach",
     "Me = 🕵️‍♂️+👨‍🔬+👨‍💻"];
 
 function randomizeFunction() {
