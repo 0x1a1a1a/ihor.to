0c8ed14171fcfb8aa31148c204c573df49610bfc
inununit <59264318+inununit@users.noreply.github.com>
2023-03-02 03:42:54 +0200

    Updated CV

--- js/randomcolor.js
+++ js/randomcolor.js
@@ -1,10 +1,10 @@
 function getColor() { 
     return "hsl(" + 360 * Math.random() + ','
-    + 50 + '%,'
-    + 95 + '%)'
+    + 20 + '%,'
+    + 30 + '%)'
 }
 
-var x = document.getElementsByClassName("block");
+var x = document.getElementsByClassName("block-color");
 
 for (var i=0; i<x.length; i++) {
     x[i].style.borderColor = getColor();
